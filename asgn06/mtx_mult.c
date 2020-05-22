b658ab894056cdb9562a803779bc0518d8140d1f
praisejimmy <ryan@ryanmyers.com>
2020-05-22 11:19:11 -0700

    Changed program for multiple matricies

--- asgn06/mtx_mult.c
+++ asgn06/mtx_mult.c
@@ -1,11 +1,13 @@
 #include "mpi.h"
 #include <stdio.h>
+
 int main(int argc, char *argv[] ) {
-    int numprocs, rank, chunk_size, i,j;
+    int numprocs, rank, chunk_size, i,j,k;
     int max, mymax,rem;
-    int matrix[800][800]; int vector[800];
-    int local_matrix[800][800]; int result[800];
-    int global_result[800];
+    int mtx1[800][800]; int mtx2[800][800];
+    int local_matrix1[800][800]; int local_matrix2; int result[800][800];
+    int global_result[800][800];
+    int seq_result[800][800];
     double t1, t2;
     MPI_Status status;
     /* Initialize MPI */
@@ -17,9 +19,25 @@ int main(int argc, char *argv[] ) {
     if (rank == 0) { /* Only on the root task... */
         /* Initialize Matrix and Vector */
         for(i=0;i<800;i++) {
-            vector[i] = i;
             for(j=0;j<800;j++) {
-                matrix[i][j] = i+j;
+                seq_result[i][j] = 0;
+                mtx1[i][j] = i+j;
+                mtx2[i][j] = i*j;
+            }
+        }
+    }
+    if (rank == 0) {
+        for(i=0;i<800;i++) {
+            for(j=0;j<800;j++) {
+                for(k=0;k<800;k++) {
+                    seq_result[i][j] = mtx1[i][k] * mtx2[k][j];
+                }
+            }
+        }
+        printf("Sequential result:\n");
+        for(i=0;i<800;i++) {
+            for(j=0;j<800;j++) {
+                printf(" %d \t ",global_result[i][j]);
             }
         }
     }
@@ -29,14 +47,20 @@ int main(int argc, char *argv[] ) {
     /* Distribute Matrix */
     /* Assume the matrix is too big to bradcast. Send blocks of rows to each task,
     nrows/nprocs to each one */
-    MPI_Scatter(matrix,800*chunk_size,MPI_INT,local_matrix,800*chunk_size,MPI_INT,
-    0,MPI_COMM_WORLD);
+    MPI_Scatter(mtx1,800*chunk_size,MPI_INT,local_matrix1,800*chunk_size,MPI_INT,
+        0,MPI_COMM_WORLD);
+
+    MPI_Scatter(mtx2,800*chunk_size,MPI_INT,local_matrix2,800*chunk_size,MPI_INT,
+        0,MPI_COMM_WORLD);
+
     /*Each processor has a chunk of rows, now multiply and build a part of the solution vector
     */
     for(i=0;i<chunk_size;i++) {
-        result[i]=0;
         for(j=0;j<800;j++) {
-            result[i] += local_matrix[i][j]*vector[j];
+            result[i][j] = 0;
+            for(k=0;k<800;k++) {
+                result[i][j] += mtx1[i][k] * mtx2[k][j]
+            }
         }
     }
     /*Send result back to master */
@@ -44,8 +68,11 @@ int main(int argc, char *argv[] ) {
     0,MPI_COMM_WORLD);
     /*Display result */
     if(rank==0) {
+        printf("Concurrent result:\n");
         for(i=0;i<800;i++) {
-            printf(" %d \t ",global_result[i]);
+            for(j=0;j<800;j++) {
+                printf(" %d \t ",global_result[i][j]);
+            }
         }
     }
     MPI_Finalize();
