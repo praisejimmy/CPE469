50d66828815105753fd55321900616839172cf65
praisejimmy <ryan@ryanmyers.com>
2020-05-25 14:38:12 -0700

    M

--- asgn06/mtx_mult.c
+++ asgn06/mtx_mult.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[] ) {
     int numprocs, rank, chunk_size, i,j,k;
     int max, mymax,rem;
     int mtx1[800][800]; int mtx2[800][800];
-    int local_matrix1[800][800]; int local_matrix2; int result[800][800];
+    int local_matrix1[800][800]; int local_matrix2[800][800]; int result[800][800];
     int global_result[800][800];
     int seq_result[800][800];
     double t1, t2;
@@ -49,9 +49,9 @@ int main(int argc, char *argv[] ) {
     /* Assume the matrix is too big to bradcast. Send blocks of rows to each task,
     nrows/nprocs to each one */
     t1 = MPI_Wtime();
-    MPI_Scatter(mtx1,800*chunk_size,MPI_INT,(void)local_matrix1,800*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Scatter(mtx1,800*chunk_size,MPI_INT,local_matrix1,800*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
 
-    MPI_Scatter(mtx2,800*chunk_size,MPI_INT,(void)local_matrix2,800*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Scatter(mtx2,800*chunk_size,MPI_INT,local_matrix2,800*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
 
     /*Each processor has a chunk of rows, now multiply and build a part of the solution vector
     */
