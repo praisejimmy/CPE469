a2aad1050f5f98c568d3a793a3b643d17fd63cc2
praisejimmy <ryan@ryanmyers.com>
2020-05-26 19:17:16 -0700

    m

--- asgn06/mtx_mult.c
+++ asgn06/mtx_mult.c
@@ -117,7 +117,7 @@ int main(int argc, char *argv[] ) {
         printf("MTX1:\n");
         for (i = 0; i < 1; i++) {
             for (j = 0; j < MTX_SIZE; j++) {
-                printf("%d\t", mtx1[i][j]);
+                printf("%d\t", local_matrix1[i][j]);
             }
             printf("\n");
         }
