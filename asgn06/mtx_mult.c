759d2a74171bf330272806203a39df3806edf918
praisejimmy <ryan@ryanmyers.com>
2020-05-26 21:58:31 -0700

    m

--- asgn06/mtx_mult.c
+++ asgn06/mtx_mult.c
@@ -104,8 +104,8 @@ int main(int argc, char *argv[] ) {
     }
     t1 = MPI_Wtime();
 
-    MPI_Scatter(&(mtx1[0][0]),MTX_SIZE*chunk_size,MPI_INT,&(local_matrix1[0][0]),MTX_SIZE*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(&(mtx2[0][0]),MTX_SIZE * MTX_SIZE,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Scatter(mtx1,MTX_SIZE*chunk_size,MPI_INT,local_matrix1,MTX_SIZE*chunk_size,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(mtx2,MTX_SIZE * MTX_SIZE,MPI_INT,0,MPI_COMM_WORLD);
     // if (rank == 0) {
     //     printf("RANK 0 BUFFER: %p\n", &local_matrix1);
     //     printf("MTX1:\n");
