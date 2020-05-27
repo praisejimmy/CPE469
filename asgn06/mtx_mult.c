7b4f384edf120c23f7551aae9ab2bca1f5630470
praisejimmy <ryan@ryanmyers.com>
2020-05-26 22:17:22 -0700

    m

--- asgn06/mtx_mult.c
+++ asgn06/mtx_mult.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define MTX_SIZE 800
+#define MTX_SIZE 400
 
 int ** allocate_array(int ** array, int rows, int cols){
     int i = 0;
@@ -29,7 +29,7 @@ int main(int argc, char *argv[] ) {
     // int ** global_result = NULL;
     // int ** seq_result = NULL;
     int mtx1[MTX_SIZE][MTX_SIZE]; int mtx2[MTX_SIZE][MTX_SIZE];
-    int local_matrix1[MTX_SIZE / 8][MTX_SIZE]; int local_matrix2[MTX_SIZE][MTX_SIZE]; int result[MTX_SIZE / 8][MTX_SIZE];
+    int local_matrix1[MTX_SIZE][MTX_SIZE]; int local_matrix2[MTX_SIZE][MTX_SIZE]; int result[MTX_SIZE][MTX_SIZE];
     int global_result[MTX_SIZE][MTX_SIZE];
     int seq_result[MTX_SIZE][MTX_SIZE];
     double t1, t2;
